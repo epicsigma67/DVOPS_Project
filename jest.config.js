a2274f7bb684442251e21e62459e09838cb02c59
Dalton <2301126E@student.tp.edu.sg>
2025-01-13 11:20:58 +0800

    Bug Fix

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleDirectories: ['node_modules', 'public/js'], // Include public/js for resolving modules
-    collectCoverageFrom: ['Util/postUtil.js']
     testEnvironment: 'jsdom', // Ensure tests run in a browser-like environment
     transform: {
         '^.+\\.jsx?$': 'babel-jest', // Use babel-jest to transform JS and JSX files
