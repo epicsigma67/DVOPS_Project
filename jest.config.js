7014381b0468431ba75fe64970232cf9ea2675a5
Dalton <2301126E@student.tp.edu.sg>
2025-01-13 11:20:03 +0800

    Fixing issue with coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleDirectories: ['node_modules', 'public/js'], // Include public/js for resolving modules
+    collectCoverageFrom: ['Util/postUtil.js']
     testEnvironment: 'jsdom', // Ensure tests run in a browser-like environment
     transform: {
         '^.+\\.jsx?$': 'babel-jest', // Use babel-jest to transform JS and JSX files
